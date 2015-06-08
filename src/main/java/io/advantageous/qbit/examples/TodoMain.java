d86f6daa1a508dc1adc6adb994d2229c1daac941
fadihub <fadimaalouli@gmail.com>
2015-06-07 19:23:42 -0700

    version 0.8.6

--- src/main/java/io/advantageous/qbit/examples/TodoMain.java
+++ src/main/java/io/advantageous/qbit/examples/TodoMain.java
@@ -1,12 +1,12 @@
 package io.advantageous.qbit.examples;
 
-import io.advantageous.qbit.server.ServiceServer;
-import io.advantageous.qbit.server.ServiceServerBuilder;
+import io.advantageous.qbit.server.ServiceEndpointServer;
+import io.advantageous.qbit.server.EndpointServerBuilder;
 
 public class TodoMain {
 
     public static void main(String... args) {
-        ServiceServer server = new ServiceServerBuilder().setHost("localhost").setPort(8080).build();
+        ServiceEndpointServer server = new EndpointServerBuilder().setHost("localhost").setPort(8080).build();
         server.initServices(new TodoService());
         server.start();
     }
